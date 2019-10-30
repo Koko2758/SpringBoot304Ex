7c5239977bb1bcf0502cc538d8bb7da798cc4aa4
Koko2758 <akosua.kankam@gmail.com>
2019-10-30 18:28:45 -0400

    SpringBoot304Ex push

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Min(4)
+    private int year;
+
+    @NotNull
+    @Size(min=2)
+    private String make;
+
+    @NotNull
+    @Size(min=2)
+    private String model;
+
+    @NotNull
+    @Size(min=10)
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
